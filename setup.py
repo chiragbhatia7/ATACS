dd747506bfceff34f44ae12a25e89d1065c0e72b
Chirag Bhatia <chiragbhatia2002@yahoo.com>
2024-11-12 23:18:51 +0530

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ATACS',
-    version='0.1.0',
+    version='0.1.1',
     author='Chirag Bhatia',
     author_email='chiragbhatia2002@yahoo.com',
     description='A Python package for financial analysis including RSI, MACD, and statistical insights.',
